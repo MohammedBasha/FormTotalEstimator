c882fbe46f97f87843e161864d8fd06288c70ad0
Mohammed Basha <mohammedbasha@bestresponsemedia.com>
2018-10-07 09:41:02 +0200

    Adding event listeners for the document, form and the state select menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,23 @@
-(function() {
-"use strict";
-	
-	
-	
-})();
+(function () {
+    "use strict";
+    
+    const cartForm = document.getElementById("cart-hplus"),
+          state = document.getElementById("s-state"),
+          etimatorBtn = document.getElementById("btn-estimate");
+    
+    function totalEstimator(e) {
+        e.preventDefault();
+        
+    }
+    
+    document.addEventListener("DOMContentLoaded", function () {
+        cartForm.addEventListener("submit", totalEstimator);
+        
+        etimatorBtn.disabled = true;
+        
+        state.addEventListener("change", function () {
+            etimatorBtn.disabled = (state.value === "");
+        });
+    });
+    
+}());
