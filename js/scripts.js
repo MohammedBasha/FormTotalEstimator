63a0eae334f44bbf719844bac44b55c44789e4ab
Mohammed Basha <mohammedbasha@bestresponsemedia.com>
2018-10-07 16:09:46 +0200

    Retrieve the data from the input fields

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,31 @@
 (function () {
     "use strict";
-    
+
     const cartForm = document.getElementById("cart-hplus"),
           state = document.getElementById("s-state"),
-          etimatorBtn = document.getElementById("btn-estimate");
-    
+          estimatorBtn = document.getElementById("btn-estimate");
+
     function totalEstimator(e) {
         e.preventDefault();
-        
+
+        const itemBall = document.getElementById("txt-q-bball").value,
+              itemJersey = document.getElementById("txt-q-jersey").value,
+              itemPower = document.getElementById("txt-q-power").value,
+              shippingState = state.value,
+              shippingMethod = document.querySelector("[name=r_method]:checked").value;
+              
+
+        console.log(itemBall, itemJersey, itemPower, shippingState, shippingMethod);
     }
-    
+
     document.addEventListener("DOMContentLoaded", function () {
         cartForm.addEventListener("submit", totalEstimator);
-        
-        etimatorBtn.disabled = true;
-        
+
+        estimatorBtn.disabled = true;
+
         state.addEventListener("change", function () {
-            etimatorBtn.disabled = (state.value === "");
+            estimatorBtn.disabled = (state.value === "");
         });
     });
-    
+
 }());
